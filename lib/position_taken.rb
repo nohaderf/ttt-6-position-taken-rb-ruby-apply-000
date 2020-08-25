55d6ee0314b30706249cc87787cd1c0444bd0ade
Freda Hon <freda.hon@gmail.com>
2020-08-25 02:45:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  if board[index] == "" || " "
+    false
+  else
+    true
+  end
+end
+
+# Struggled to return true if the board has a value of "X" or "O in the index"
