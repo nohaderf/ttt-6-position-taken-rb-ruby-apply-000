cddf146acb637c160a096a739adc2802765e3f6f
Freda Hon <freda.hon@gmail.com>
2020-08-25 02:50:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -9,6 +9,7 @@ def position_taken?(board, index)
 end
 
 # Struggled to return true if the board has a value of "X" or "O in the index"
+# I tried playing around with my else statement, but that didn't work
 # Realized it was improper to write:
 #   if board[index] == "" || == " "
 # Test still failed though with the failure to return false if the board has nil in the index
